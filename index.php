c4c7304615c487f62f785cc6f81e47892ced1e0b
account931 <account931@ukr.net>
2019-10-01 14:41:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 
 	<?php
-	//ini_set('display_errors', 1);
-    //ini_set('display_startup_errors', 1);
+        //enable debugger
+	ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
     error_reporting(1);  //1-> turn on errors; 0 -> off
 	
 	
